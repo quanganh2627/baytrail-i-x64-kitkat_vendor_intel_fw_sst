94976fab75ee4c3d9369a3616f9e98763495cc01
Erwan Mahe <erwanx.mahe@intel.com>
2011-06-21 22:31:15 +0200

    add Android.mk
    BZ: 3559
    split common firmware project
    keep history as much as possible
    
    Change-Id: I7f074d81219606d6472a83e222c1b65d9ae3b06d
    Signed-off-by: Erwan Mahe <erwanx.mahe@intel.com>

---
+++ Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/fw_sst.bin:system/etc/firmware/fw_sst.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/fw_sst_082f.bin:system/etc/firmware/fw_sst_082f.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/aac_dec_1.bin:system/etc/firmware/aac_dec_1.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/aac_enc_1.bin:system/etc/firmware/aac_enc_1.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/aac_v1_dec_1.bin:system/etc/firmware/aac_v1_dec_1.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/aac_v2_dec_1.bin:system/etc/firmware/aac_v2_dec_1.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/mp3_dec_1.bin:system/etc/firmware/mp3_dec_1.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/wma9_dec_1.bin:system/etc/firmware/wma9_dec_1.bin
+
+
