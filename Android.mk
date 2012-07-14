18215985859146ac37fa83ee6c34c257d59021e2
Bruce Beare <bruce.j.beare@intel.com>
2012-07-14 00:03:11 -0700

    PRODUCT_COPY_FILES: Disable incorrect use
    BZ: 46719
    
    This makfile variable is only able to be set in "product.mk" files -- never
    in Android.mk or AndroidBoard.mk files.
    
    Change-Id: Ia533e9767cf9f496cc28114705fc55d703183e73

--- Android.mk
+++ Android.mk
@@ -1,21 +1,20 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/fw_sst_082f.bin:system/etc/firmware/fw_sst_082f.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/fw_sst_08e7.bin:system/etc/firmware/fw_sst_08e7.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/aac_dec_1.bin:system/etc/firmware/aac_dec_1.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/aac_enc_1.bin:system/etc/firmware/aac_enc_1.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/aac_v1_dec_1.bin:system/etc/firmware/aac_v1_dec_1.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/aac_v2_dec_1.bin:system/etc/firmware/aac_v2_dec_1.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/mp3_dec_1.bin:system/etc/firmware/mp3_dec_1.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/wma9_dec_1.bin:system/etc/firmware/wma9_dec_1.bin
-
-
+$(info WARNING: PRODUCT_COPY_FILES disabled in $(LOCAL_PATH))
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/fw_sst_082f.bin:system/etc/firmware/fw_sst_082f.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/fw_sst_08e7.bin:system/etc/firmware/fw_sst_08e7.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/aac_dec_1.bin:system/etc/firmware/aac_dec_1.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/aac_enc_1.bin:system/etc/firmware/aac_enc_1.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/aac_v1_dec_1.bin:system/etc/firmware/aac_v1_dec_1.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/aac_v2_dec_1.bin:system/etc/firmware/aac_v2_dec_1.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/mp3_dec_1.bin:system/etc/firmware/mp3_dec_1.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/wma9_dec_1.bin:system/etc/firmware/wma9_dec_1.bin
